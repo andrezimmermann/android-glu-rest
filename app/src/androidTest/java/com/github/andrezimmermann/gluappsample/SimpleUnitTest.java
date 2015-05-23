095b064a5bfd5eb6c2a14df264f271e18542c917
André Zimmermann <andrezimmermann@email.com>
2015-05-23 17:32:58 -0300

    Initial Commit

---
+++ app/src/androidTest/java/com/github/andrezimmermann/gluappsample/SimpleUnitTest.java
@@ -0,0 +1,27 @@
+package com.github.andrezimmermann.gluappsample;
+
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.SmallTest;
+
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Sample of a Test Case
+ */
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class SimpleUnitTest extends TestCase {
+
+    @Test
+    public void testCase001(){
+
+        assertTrue(true);
+
+
+    }
+
+}
