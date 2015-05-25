7ff0ed69687722d67dbba4f751ea94dc19eacabf
André Zimmermann <andrezimmermann@email.com>
2015-05-25 01:18:49 -0300

    Details implemented

--- app/src/main/java/com/github/andrezimmermann/gluappsample/client/DetailActivity.java
+++ app/src/main/java/com/github/andrezimmermann/gluappsample/client/DetailActivity.java
@@ -7,15 +7,16 @@ import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import com.github.andrezimmermann.gluappsample.R;
 import com.github.andrezimmermann.gluappsample.client.detail.Presenter;
 import com.github.andrezimmermann.gluappsample.client.detail.RouteDetailView;
 import com.github.andrezimmermann.gluappsample.client.service.BusDetail;
 import com.github.andrezimmermann.gluappsample.client.service.GluApiSingleton;
-import com.github.andrezimmermann.gluappsample.client.widgets.ListUtils;
 import com.github.andrezimmermann.gluappsample.client.widgets.SimpleAdapter;
 import com.github.andrezimmermann.gluappsample.shared.data.BusDeparture;
 import com.github.andrezimmermann.gluappsample.shared.data.BusStop;
@@ -25,8 +26,16 @@ import java.util.List;
 
 public class DetailActivity extends ActionBarActivity implements RouteDetailView {
 
+    public static final int EMPTY_ROUTE_ID = -1;
+
     private Presenter presenter;
     private ProgressDialog progressDialog;
+
+    private int routeId = EMPTY_ROUTE_ID;
+
+
+    private ViewFlipper viewFlipper;
+    private float lastX;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,19 +44,71 @@ public class DetailActivity extends ActionBarActivity implements RouteDetailView
 
         presenter = new Presenter(this, GluApiSingleton.getInstance());
 
+        viewFlipper = (ViewFlipper) findViewById(R.id.detail_flipper);
 
         Intent intent = getIntent();
 
         int routeId = intent.getIntExtra(RouteListActivity.ROUTE_ID, 0);
         String fullDescription = intent.getStringExtra(RouteListActivity.FULL_DESCRIPTION);
 
+        if (shouldFetchDetail(routeId, this.routeId)) {
+            processRouteId(routeId, fullDescription);
+        }
+
+    }
+
+    // Method to handle touch event like left to right swap and right to left swap
+    public boolean onTouchEvent(MotionEvent touchevent) {
+        switch (touchevent.getAction()) {
+            // when user first touches the screen to swap
+            case MotionEvent.ACTION_DOWN: {
+                lastX = touchevent.getX();
+                break;
+            }
+            case MotionEvent.ACTION_UP: {
+                float currentX = touchevent.getX();
+
+                // if left to right swipe on screen
+                if (lastX < currentX) {
+                    // If no more View/Child to flip
+                    if (viewFlipper.getDisplayedChild() == 0)
+                        break;
+
+                    // set the required Animation type to ViewFlipper
+                    // The Next screen will come in form Left and current Screen will go OUT from Right
+                    viewFlipper.setInAnimation(this, R.anim.in_from_left);
+                    viewFlipper.setOutAnimation(this, R.anim.out_to_right);
+                    // Show the next Screen
+                    viewFlipper.showNext();
+                }
+
+                // if right to left swipe on screen
+                if (lastX > currentX) {
+                    if (viewFlipper.getDisplayedChild() == 1)
+                        break;
+                    // set the required Animation type to ViewFlipper
+                    // The Next screen will come in form Right and current Screen will go OUT from Left
+                    viewFlipper.setInAnimation(this, R.anim.in_from_right);
+                    viewFlipper.setOutAnimation(this, R.anim.out_to_left);
+                    // Show The Previous Screen
+                    viewFlipper.showPrevious();
+                }
+                break;
+            }
+        }
+        return false;
+    }
+
+    private boolean shouldFetchDetail(int currentRouteId, int oldRouteId) {
+        return currentRouteId != oldRouteId;
+    }
+
+    private void processRouteId(int routeId, String fullDescription) {
         if (routeId != 0) {
             presenter.onDetailRequest(routeId, fullDescription);
         } else {
             backToListView();
         }
-
-
     }
 
     private void backToListView() {
@@ -93,14 +154,9 @@ public class DetailActivity extends ActionBarActivity implements RouteDetailView
 
         renderDepartures(result.getDepartures());
         renderStops(result.getStops());
-        calculateHeight();
 
     }
 
-    private void calculateHeight() {
-        ListUtils.setDynamicHeight(getStopsList());
-        ListUtils.setDynamicHeight(getDeparturesList());
-    }
 
     private void renderStops(List<BusStop> result) {
         ListView stopsView = getStopsList();
